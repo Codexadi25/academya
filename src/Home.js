30276f8bef8bda7bd9635d8a09f63d5ffbb7957b
Codexadi25 <111916672+Codexadi25@users.noreply.github.com>
2024-03-05 22:25:43 +0530

    modified:   package-lock.json 	modified:   src/Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './Home.css';
 import banner from './banner.jpeg'
-import Typing from 'react-typed';
+import { ReactTyped } from "react-typed";
 
 
 function Home() {
   return (
     <div className='home'>
       <img src={banner} className='banner' alt='Academya-banner'/>
+      <div className='Tagline'>
+        <h1>Academya | </h1>
+        <ReactTyped
+          strings={[
+            "E-Learning",  
+            "Tution",
+            "Coaching",
+            "Consultancy",
+          ]}
+          typeSpeed={40}
+          backSpeed={50}
+          attr="placeholder"
+          loop></ReactTyped>
+      </div>
     </div>
   )
 }
